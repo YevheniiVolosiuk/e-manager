0135da432681b1f920a088752186fdbe593c0dc8
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2023-03-22 00:27:26 +0100

    configured Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'employees';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'birth_date',
+        'hire_date',
+    ];
+
+    public function salary()
+    {
+        return $this->belongsTo(Salary::class);
+    }
+
+    public function title()
+    {
+        return $this->belongsTo(Title::class);
+    }
+
+    public function departments()
+    {
+        return $this->belongsToMany(Department::class, 'department_employee', 'employee_id', 'id');
+    }
+
+    public function managers()
+    {
+        return $this->belongsToMany(Department::class, 'department_manager', 'employee_id', 'id');
+    }
 }
