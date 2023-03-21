9160f44fe2f3f5d5e7b8856dc5f53d8771b9d4cc
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2023-03-21 23:00:49 +0100

    add columns for employee migration

--- database/migrations/2023_03_21_213941_create_employees_table.php
+++ database/migrations/2023_03_21_213941_create_employees_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamp('birth_date')->nullable();
+            $table->enum('gender', ['M', 'F'])->nullable();
+            $table->timestamp('hire_date');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->index(['first_name', 'last_name']);
         });
     }
 
